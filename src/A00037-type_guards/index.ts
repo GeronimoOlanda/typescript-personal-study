57d9b7529450b748198bd2fff020fd1ca8ad7dfa
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-09-08 09:56:39 -0300

    Refinando o tipo, type guard

--- src/A00037-type_guards/index.ts
+++ src/A00037-type_guards/index.ts
@@ -5,16 +5,29 @@ function add(a: unknown, b: unknown): number | string {
 console.log(add(1, 2));
 console.log(add('gg', '22'));
 
-type Pessoa = { nome: string };
-type Animal = { cor: string };
+type Pessoa = { tipo: 'pessoa'; nome: string };
+type Animal = { tipo: 'animal'; cor: string };
 type PessoaOuAnimal = Pessoa | Animal;
 
 export class Aluno implements Pessoa {
   constructor(public nome: string) {}
+  tipo: 'pessoa' = 'pessoa';
 }
 
 function mostraNome(obj: PessoaOuAnimal) {
   //if ('nome' in obj) console.log(obj.nome);
-  if (obj instanceof Aluno) console.log(obj.nome);
+  //if (obj instanceof Aluno) console.log(obj.nome);
+
+  switch (obj.tipo) {
+    case 'pessoa':
+      console.log(obj.nome);
+      return;
+
+    case 'animal':
+      console.log('Esse é um animal', obj.cor);
+      return;
+  }
 }
+
 mostraNome(new Aluno('Geronimo'));
+mostraNome({ tipo: 'animal', cor: 'Rosa' });
