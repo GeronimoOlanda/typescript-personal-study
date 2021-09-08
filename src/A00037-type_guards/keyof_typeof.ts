7b290dd4ee9d8dc89aac79d0acc1481d11a875bf
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-09-08 10:08:47 -0300

    Estamos utilizando keyof para setar os valores de forma dinamica

--- src/A00037-type_guards/keyof_typeof.ts
+++ src/A00037-type_guards/keyof_typeof.ts
@@ -1,15 +1,19 @@
 type CoresObj = typeof coresObj;
-
+type CoresChaves = keyof CoresObj;
 const coresObj = {
   vermelho: 'red',
   verde: 'green',
   azul: 'blue',
+  roxo: 'purple',
+  cinza: 'gray',
 };
 
-function traduzirCor(cor: 'vermelho' | 'verde' | 'azul', cores: CoresObj) {
+function traduzirCor(cor: CoresChaves, cores: CoresObj) {
   return cores[cor];
 }
 
 console.log(traduzirCor('azul', coresObj));
 console.log(traduzirCor('vermelho', coresObj));
 console.log(traduzirCor('verde', coresObj));
+console.log(traduzirCor('roxo', coresObj));
+console.log(traduzirCor('cinza', coresObj));
