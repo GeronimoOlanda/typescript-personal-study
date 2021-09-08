a6e710ce9078413495494493607907be52b571b8
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-09-08 10:06:57 -0300

    Utilizando typeof para instanciar um type de forma dinamica

---
+++ src/A00037-type_guards/keyof_typeof.ts
@@ -0,0 +1,15 @@
+type CoresObj = typeof coresObj;
+
+const coresObj = {
+  vermelho: 'red',
+  verde: 'green',
+  azul: 'blue',
+};
+
+function traduzirCor(cor: 'vermelho' | 'verde' | 'azul', cores: CoresObj) {
+  return cores[cor];
+}
+
+console.log(traduzirCor('azul', coresObj));
+console.log(traduzirCor('vermelho', coresObj));
+console.log(traduzirCor('verde', coresObj));
