34070da21db3ddcdb68cc691958c158d95e15cd5
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-09-08 09:52:13 -0300

    Checagem nativa

--- src/A00037-type_guards/index.ts
+++ src/A00037-type_guards/index.ts
@@ -7,7 +7,14 @@ console.log(add('gg', '22'));
 
 type Pessoa = { nome: string };
 type Animal = { cor: string };
+type PessoaOuAnimal = Pessoa | Animal;
 
 export class Aluno implements Pessoa {
   constructor(public nome: string) {}
 }
+
+function mostraNome(obj: PessoaOuAnimal) {
+  //if ('nome' in obj) console.log(obj.nome);
+  if (obj instanceof Aluno) console.log(obj.nome);
+}
+mostraNome(new Aluno('Geronimo'));
