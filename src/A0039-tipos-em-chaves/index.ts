38d3009b771a513db94c11911aa7be8ce0a8d689
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-09-14 01:46:24 -0300

    Associando a tipagem por meio das chaves

--- src/A0039-tipos-em-chaves/index.ts
+++ src/A0039-tipos-em-chaves/index.ts
@@ -3,7 +3,16 @@ type Veiculo = {
   ano: string;
 };
 
-type Cara = {
-  brand: string;
-  year: string;
+type Car = {
+  brand: Veiculo['marca']; // estamos pegando o tipo de veiculo que é a marca, e esta tipada como string
+  year: Veiculo['ano'];
+  name: string;
 };
+
+const carro: Car = {
+  brand: 'Ford',
+  year: '2019',
+  name: 'Diferenciado',
+};
+
+console.log(carro);
