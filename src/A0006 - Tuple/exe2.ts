9af1234381d4611596253528acb3263652bfa88f
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-10-02 23:39:22 -0300

    revisando editorconfig e treinando tuples em ts

---
+++ src/A0006 - Tuple/exe2.ts
@@ -0,0 +1,10 @@
+const dadosFuncionarios: [string, string] = ['Thalia', 'Olanda'];
+const habilidadesNecessarias: [string, string, string] = [
+  'JavaScript',
+  'NodeJs',
+  'ReactJs',
+];
+
+dadosFuncionarios[1] = 'MyWifee';
+
+console.log(dadosFuncionarios);
