982d15c147af7612708c5f38b508ac742b586c92
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-10-12 09:51:20 -0300

    Utilizando this como tipo em um padrao da GoF

--- src/A040-this-polimorfico/index.ts
+++ src/A040-this-polimorfico/index.ts
@@ -1,3 +1,4 @@
+//utilizando this como tipo de dado
 export class Calculator {
   constructor(public number: number) {}
   add1(n: number): this {
@@ -18,5 +19,30 @@ export class Calculator {
   }
 }
 
+export class SubCalculator extends Calculator {
+  pow(n: number): this {
+    this.number **= n;
+    return this;
+  }
+}
 const cal = new Calculator(10);
-console.log(cal.add1(10).mult1(2));
+console.log(cal.add1(10).mult1(2).div1(2));
+
+const subCal = new SubCalculator(10);
+console.log(subCal.add1(10).mult1(2).div1(2).pow(2));
+
+//Builder - GoF
+export class RequestBuilder {
+  private method: 'get' | 'post' | null = null;
+  private url: string | null = null;
+
+  setMethod(method: 'get' | 'post'): this {
+    this.method = method;
+    return this;
+  }
+
+  setUrl(url: 'get' | 'post'): this {
+    this.url = url;
+    return this;
+  }
+}
